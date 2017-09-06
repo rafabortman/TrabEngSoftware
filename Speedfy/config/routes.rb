289bce5f0fcab81a7b5d3bd99020ebc9f940a40a
henrique bortoletti <henriquebortoletti@gmail.com>
2017-09-06 03:27:28 -0300

    teste

--- Speedfy/config/routes.rb
+++ Speedfy/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :jogos
   resources :amigos
+  root 'application#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
