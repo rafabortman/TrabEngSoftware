13102101a299e368d70de0901d059b0e99b42d0e
henriquebortoletti <henriquebortoletti@gmail.com>
2017-09-08 02:34:03 -0300

    home alterada

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-	render html: "Welcome to speedfy"
+render html: '<h1>Welcome to<a style="color:GoldenRod"> Speedfy</a></h1>
+<h2>Our pages:</h2>
+	<ul>
+			<li><a href="/jogadas">jogadas</a></li>
+			<li><a href="/jogos">jogos</a></li>
+			<li><a href="/amigos">amigos</a></li>
+			<li><a href="/usuarios">usuarios</a></li>
+</ul>
+
+'.html_safe
 end
 
 end
