7a406519388a571514a29c51fb2a65a0fd63c84a
Mariana <marianasoaresviana2@gmail.com>
2017-09-18 19:12:13 -0300

    Update usuario.rb

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -8,4 +8,7 @@ class Usuario < ApplicationRecord
  before_save { self.email = email.downcase }
     validates :email, presence: true, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create}, uniqueness: {case_sensitive: false}
  
+    validates :nome, presence: true, length: { maximum: 50 }
+    validates :senha, presence: true, length: { minimum: 6 }
+    
 end
