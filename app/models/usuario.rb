992fff61b29a2c5a290522864e81d78c2d8063a1
Mariana <marianasoaresviana2@gmail.com>
2017-09-21 23:10:53 -0300

    Validação dos campos

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,2 +1,10 @@
 class Usuario < ApplicationRecord
+  
+  validates_presence_of :nome, message: "Campo nome deve ser preenchido"
+  validates_presence_of :email, message: "Campo de e-mail deve ser preenchido"
+  validates_presence_of :username, message: "Campo de usuário deve ser preenchido"
+  validates_presence_of :senha, message: "Campo de senha deve ser preenchido"
+      
+  validates :email, format: {with: /@/}
+  
 end
