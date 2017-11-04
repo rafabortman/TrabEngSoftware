3bb50fdc429a36fc16339d404717a1d2dbbfe93b
Henrique <henriquebortoletti@gmail.com>
2017-11-04 02:00:34 -0200

    user email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,15 +91,15 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
-  config.action_mailer.smtp_settings = {
-   :address              => "smtp.gmail.com",
-   :port                 => 587,
-   :user_name            => ENV['gmail_username'],
-   :password             => ENV['gmail_password'],
-   :authentication       => "plain",
-   :enable_starttls_auto => true
+config.action_mailer.default_url_options = { :host => "localhost" }
+config.action_mailer.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :domain => 'localhost:80',
+      :user_name => ENV['gmail_username'],
+      :password => ENV['gmail_password'],
+      :authentication => "plain",
+      :enable_starttls_auto => true
 }
-
-
 
 end
